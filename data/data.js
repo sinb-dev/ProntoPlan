95d6e69aaa49d4772d823c7ac54399f79d5f7da9
simonhoxer <simonhoxer@gmail.com>
2021-11-23 19:01:17 +0100

    Added vue3

---
+++ data/data.js
@@ -0,0 +1,39 @@
+function get_data() { return {
+    line : 'pd',
+    teachers : [
+        'sinb', 'fros', 'lpte', 'lkri', 'henk'
+    ],
+    maincourse : [{
+        name : "H5",
+        duration : 50,
+        courses : [
+            { 
+                name : 'Serverside programmering III',
+                short : 'Serverside',
+                primary_teacher : 'lpte',
+                secondary_teacher : 'sinb',
+                duration : 15
+            },
+            { 
+                name : 'Systemudvikling og projektstyring',
+                short : 'Sys.udv.',
+                primary_teacher : 'sinb',
+                secondary_teacher : 'fros',
+                duration : 15
+            }
+        ]   
+    }],
+    allocation : {
+        'H5' : {
+            'week_1' : {
+                'monday' : {
+                    morning: 'Serverside',
+                    afternoon: '',
+                    primary_teacher: '',
+                    secondary_teacher: '',
+                    note: ''
+                }
+            }
+        }
+    }
+}}
